a86bd9beb84e2f9b426273ee1bd2f0e7ec6b14a2
Quotermain <pavelhat@gmail.com>
2019-05-14 14:12:46 +0500

    Rewrites the whole file. Need to write only the last line.

--- additional_scripts/prices_to_csv.lua
+++ additional_scripts/prices_to_csv.lua
@@ -9,16 +9,48 @@ function split_string(inputstr, sep)
         return t
 end
 
+--file = "C:/Users/Quotermain233/Desktop/VBShared/"..asset.."/"..tostring(period).."_"..asset..".csv"
+file = "C:/Users/Quotermain233/Desktop/VBShared/ALRS/1_ALRS.csv"
+lines_of_file = {}
+for line in io.lines(file) do
+	lines_of_file[#lines_of_file + 1] = line
+end
+last_line = lines_of_file[#lines_of_file]
+file = io.open(file, 'w+')
+
 function OnInit()
 	function Run(asset, period)
-		file = "C:/Users/Quotermain233/Desktop/VBShared/"..asset.."/"..tostring(period).."_"..asset..".csv"
-		lines_of_file = {}
-		for line in io.lines(file) do
-			lines_of_file[#lines_of_file + 1] = line
+		ds1 = CreateDataSource('TQBR', asset, INTERVAL_M1)
+		ds1:SetEmptyCallback()
+	
+		local new_line = tostring(ds1:T(ds1:Size()).year..'-'..
+									ds1:T(ds1:Size()).month..'-'..
+									ds1:T(ds1:Size()).day..' '..
+									ds1:T(ds1:Size()).hour..':'..
+									ds1:T(ds1:Size()).min..':'..
+									ds1:T(ds1:Size()).sec
+									)..','..
+							tostring(ds1:H(ds1:Size()))..','..
+							tostring(ds1:L(ds1:Size()))..','..
+							tostring(ds1:O(ds1:Size()))..','..
+							tostring(ds1:C(ds1:Size()))..','..
+							tostring(ds1:V(ds1:Size()))
+		last_line = new_line		
+		if split_string(new_line, ',')[0] == split_string(last_line, ',')[0] then
+			lines_of_file[#lines_of_file] = last_line
+		elseif split_string(new_line, ',')[0] ~= split_string(last_line, ',')[0] then
+			table.insert(lines_of_file, last_line)
+			table.remove(lines_of_file, 1)
 		end
-		last_string = lines_of_file[#lines_of_file]
-		message(tostring(split_string(last_string, ',')[3]))
+		
+		
+		for i=1, #lines_of_file do
+			file:write(lines_of_file[i]..'\n')
+		end
+		file:flush()
+		--message(new_line)
 		sleep(2000)
+		
 	end
 end
 
@@ -31,6 +63,7 @@ function main()
 end
 
 function OnStop()
+	file:close()
 	IsRun = false
 end
 
