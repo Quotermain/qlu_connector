333ca83ed54cf47fab023549f68c51b80d526e00
Quotermain <pavelhat@gmail.com>
2019-05-26 17:27:26 +0500

    Adds function for writing depo limits into csv

---
+++ additional_scripts/depo_limits_writer.lua
@@ -0,0 +1,20 @@
+IsRun = true
+
+function main ()
+	while IsRun do
+		CSV1 = io.open("C:/Users/Quotermain233/Desktop/VBShared/test/depo_limits.csv", "w+")
+		for i=0,getNumberOf("depo_limits")-1 do
+			t=getItem("depo_limits",i)
+			local line = tostring(t.sec_code)..','..tostring(t.currentbal)..'\n'
+			CSV1:write(line)
+		end
+		CSV1:flush()
+		message('done')
+		sleep(100)
+	end
+end
+
+function OnStop()
+	IsRun = false
+	CSV1:close()
+end
