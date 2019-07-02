02b0dd45c19fcac0d3f2f5fd95731f6ad7e2161c
Quotermain <pavelhat@gmail.com>
2019-07-02 12:44:53 +0500

    Adds logic for stops and orders according db

--- main3_SQL.lua
+++ main3_SQL.lua
@@ -15,10 +15,10 @@ env = luasql.mysql()
 conn = assert(env:connect("trading_data", "Quotermain233", "Quotermain233", "192.168.0.105", 3306))
 
 assets = {
-		"MGNT",
+		--[["MGNT",
 		"TATN",
 		"SBERP",
-		"MTLR",
+		"MTLR",]]
 		"ALRS",
 		"SBER",
 		"MOEX",
@@ -40,17 +40,6 @@ assets = {
 }
 
 function run(asset)
-	Money=getPortfolioInfoEx('MC0139600000','OPEN51085',2).portfolio_value
-	lot_size = getLotSizeBySecCode(asset)
-	price_step = PRICE_STEP(asset)
-	--[[dist_to_stop = tonumber(signal[1][3])
-	lots_for_trade = math.floor(Money * 0.0001 / (dist_to_stop * lot_size))
-	stakan=getQuoteLevel2("TQBR",asset)
-	if stakan~=nil then
-		price_to_buy=stakan.offer[1].price
-		price_to_sell=stakan.bid[10].price
-	end]]
-	limits, price = depo_limits(asset)
 	
 	cur = assert(
 		conn:execute(
@@ -61,10 +50,87 @@ function run(asset)
 		)
 	)
 	row = cur:fetch({}, 'a')
-	
-	message(tostring(row.dist_to_max))
 
-	sleep(1000)
+	Money=getPortfolioInfoEx('MC0139600000','OPEN51085',2).portfolio_value
+	lot_size = getLotSizeBySecCode(asset)
+	price_step = PRICE_STEP(asset)
+	stakan=getQuoteLevel2("TQBR",asset)
+	if stakan~=nil then
+		price_to_buy=stakan.offer[1].price
+		price_to_sell=stakan.bid[10].price
+	end
+	limits, price = depo_limits(asset)
+	
+	--message(tostring(math.floor(Money * 0.0001 / (tonumber(row.dist_to_max) * lot_size))))
+	--sleep(5000)
+	
+	if (limits == 0 or limits == nil) 
+	and (order_limits(asset) == 0 or order_limits(asset) == nil) then
+		if row.signal == 'short' then
+			dist_to_stop = tonumber(row.dist_to_max)
+			lots_for_trade = math.floor(Money * 0.0001 / (dist_to_stop * lot_size))
+			sendTransaction(
+				trans(
+					asset, 'S', 
+					price_to_sell - 100 * price_step,
+					lots_for_trade
+				)
+			)
+		elseif row.signal == 'long' then
+			dist_to_stop = tonumber(row.dist_to_min)
+			lots_for_trade = math.floor(Money * 0.0001 / (dist_to_stop * lot_size))
+			sendTransaction(
+				trans(
+					asset, 'B', 
+					price_to_buy + 100 * price_step,
+					lots_for_trade
+				)
+			)			
+		end
+	end
+	
+	limits, price = depo_limits(asset)
+	lot_size = getLotSizeBySecCode(asset)
+	price_step = PRICE_STEP(asset)
+	if limits ~= 0 and (quantity_in_stop(asset) == 0 or quantity_in_stop(asset) == nil) then
+		if limits < 0 then
+			dist_to_stop = tonumber(row.dist_to_max) - math.fmod(tonumber(row.dist_to_max), price_step)
+			dist_to_profit = tonumber(row.dist_to_min) - math.fmod(tonumber(row.dist_to_min), price_step)
+			sendTransaction(
+				stop_trans(
+					'B', price - dist_to_profit, 
+					math.abs(limits) / lot_size, 
+					price + dist_to_stop + 10 * price_step, 
+					price + dist_to_stop, 
+					asset, price_step
+				)
+			)
+		elseif limits > 0 then
+			dist_to_stop = tonumber(row.dist_to_min) - math.fmod(tonumber(row.dist_to_min), price_step)
+			dist_to_profit = tonumber(row.dist_to_max) - math.fmod(tonumber(row.dist_to_max), price_step)
+			sendTransaction(
+				stop_trans(
+					'S', price + dist_to_profit, 
+					math.abs(limits) / lot_size, 
+					price - dist_to_stop - 10 * price_step, 
+					price - dist_to_stop, 
+					asset, price_step
+				)
+			)
+		end
+		sleep(1000)
+		res = assert(
+			conn:execute(
+				string.format(
+					[[UPDATE trade_signals 
+					SET `signal` = NULL, `dist_to_max` = NULL, `dist_to_min` = NULL 
+					WHERE `asset` = '%s']], 
+					asset
+				)
+			)
+		)
+	end
+	sleep(100)
 end
 
 IsRun = true
