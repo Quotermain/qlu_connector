636a5a4c720387d6de03db4e7a52eba28bded85c
Quotermain <pavelhat@gmail.com>
2019-07-08 15:31:06 +0500

    Adds flags to not close positions right after opening

--- main3_SQL.lua
+++ main3_SQL.lua
@@ -15,10 +15,10 @@ env = luasql.mysql()
 conn = assert(env:connect("trading_data", "Quotermain233", "Quotermain233", "192.168.0.105", 3306))
 
 assets = {
-		--[["MGNT",
+		"MGNT",
 		"TATN",
 		"SBERP",
-		"MTLR",]]
+		"MTLR",
 		"ALRS",
 		"SBER",
 		"MOEX",
@@ -27,7 +27,7 @@ assets = {
 		"LKOH",
 		"SIBN",
 		"GMKN",
-		"RTKM",
+		--"RTKM",
 		"SNGS",
 		"SNGSP",
 		"CHMF",
@@ -39,7 +39,35 @@ assets = {
 		"YNDX"
 }
 
+flags_to_close_if_no_stop = {
+	["MGNT"] = false,
+	["TATN"] = false,
+	["SBERP"] = false,
+	["MTLR"] = false,
+	["ALRS"] = false,
+	["SBER"] = false,
+	["MOEX"] = false,
+	["HYDR"] = false,
+	["ROSN"] = false,
+	["LKOH"] = false,
+	["SIBN"] = false,
+	["GMKN"] = false,
+	--["RTKM"] = false,
+	["SNGS"] = false,
+	["SNGSP"] = false,
+	["CHMF"] = false,
+	--["VTBR"] = false,
+	["NVTK"] = false,
+	["GAZP"] = false,
+	--["MSNG"] = false,
+	["MTSS"] = false,
+	["YNDX"] = false
+}
+
 function run(asset)
+	
+	--message(asset)
+	--sleep(2000)
 	
 	cur = assert(
 		conn:execute(
@@ -64,11 +92,12 @@ function run(asset)
 	--message(tostring(math.floor(Money * 0.0001 / (tonumber(row.dist_to_max) * lot_size))))
 	--sleep(5000)
 	
+	--Opening position
 	if (limits == 0 or limits == nil) 
 	and (order_limits(asset) == 0 or order_limits(asset) == nil) then
 		if row.signal == 'short' then
 			dist_to_stop = tonumber(row.dist_to_max)
-			lots_for_trade = math.floor(Money * 0.0001 / (dist_to_stop * lot_size))
+			lots_for_trade = math.floor(10 / (dist_to_stop * lot_size))
 			sendTransaction(
 				trans(
 					asset, 'S', 
@@ -78,7 +107,7 @@ function run(asset)
 			)
 		elseif row.signal == 'long' then
 			dist_to_stop = tonumber(row.dist_to_min)
-			lots_for_trade = math.floor(Money * 0.0001 / (dist_to_stop * lot_size))
+			lots_for_trade = math.floor(10 / (dist_to_stop * lot_size))
 			sendTransaction(
 				trans(
 					asset, 'B', 
@@ -87,8 +116,10 @@ function run(asset)
 				)
 			)			
 		end
+		sleep(2000)
 	end
 	
+	--Sending stops
 	limits, price = depo_limits(asset)
 	lot_size = getLotSizeBySecCode(asset)
 	price_step = PRICE_STEP(asset)
@@ -130,6 +161,72 @@ function run(asset)
 			)
 		)
 	end
+	
+	--Closing position according signal
+	lot_size = getLotSizeBySecCode(asset)
+	price_step = PRICE_STEP(asset)
+	stakan=getQuoteLevel2("TQBR",asset)
+	if stakan~=nil then
+		price_to_buy=stakan.offer[1].price
+		price_to_sell=stakan.bid[10].price
+	end
+	limits, price = depo_limits(asset)
+	if limits > 0 
+	and (order_limits(asset) == 0 or order_limits(asset) == nil) 
+	and (row.signal == 'short' or row.signal == 'nothing') then
+		sendTransaction(
+			trans(
+				asset, 'S', 
+				price_to_sell - 100 * price_step,
+				math.abs(limits) / lot_size
+			)
+		)
+	elseif limits < 0 
+	and (order_limits(asset) == 0 or order_limits(asset) == nil) 
+	and (row.signal == 'long' or row.signal == 'nothing') then
+		sendTransaction(
+			trans(
+				asset, 'B', 
+				price_to_buy + 100 * price_step,
+				math.abs(limits) / lot_size
+			)
+		)			
+	end
+	
+	--Closing position if no stops
+	if flags_to_close_if_no_stop[asset] == true then
+		limits, price = depo_limits(asset)
+		stakan=getQuoteLevel2("TQBR",asset)
+		if stakan~=nil then
+			price_to_buy=stakan.offer[1].price
+			price_to_sell=stakan.bid[10].price
+		end
+		if limits > 0 
+		and (order_limits(asset) == 0 or order_limits(asset) == nil)
+		and (quantity_in_stop(asset) == 0 or quantity_in_stop(asset) == nil) 	then
+			sendTransaction(
+				trans(
+					asset, 'S', 
+					price_to_sell - 100 * price_step,
+					math.abs(limits) / lot_size
+				)
+			)
+		elseif limits < 0 
+		and (order_limits(asset) == 0 or order_limits(asset) == nil) 
+		and (quantity_in_stop(asset) == 0 or quantity_in_stop(asset) == nil) then
+			sendTransaction(
+				trans(
+					asset, 'B', 
+					price_to_buy + 100 * price_step,
+					math.abs(limits) / lot_size
+				)
+			)			
+		end
+		flags_to_close_if_no_stop[asset] = false
+	end
+	
+	flags_to_close_if_no_stop[asset] = true
+	
 	sleep(100)
 end
 
