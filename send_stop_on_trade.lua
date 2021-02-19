0028864b28836945cd24c89465239bdefe9cff03
Quotermain <>
2021-02-19 21:50:17 +0500

    Adds logic for sending stop

--- send_stop_on_trade.lua
+++ send_stop_on_trade.lua
@@ -2,6 +2,7 @@ require 'utils/get_distances_to_stop'
 require 'utils/get_depo_limits'
 require 'utils/get_stop_orders'
 require 'utils/string_split'
+require 'utils/stop_trans'
 
 dist_to_stop = {}
 depo_limits = {}
@@ -43,6 +44,53 @@ function main()
           tostring(value['lot_size'])..'. Position price: '..
           tostring(value['position_price'])
         )
+
+        if value['bal'] < 0 then
+          stop_limit_price = (
+            value['position_price'] + dist_to_stop[key] * value['position_price']
+          )
+    			stop_limit_price = (
+            stop_limit_price - math.fmod(stop_limit_price, value['price_step'])
+          )
+    			take_profit_price = (
+            value['position_price'] - dist_to_stop[key] * value['position_price']
+          )
+    			take_profit_price = (
+            take_profit_price - math.fmod(take_profit_price, value['price_step'])
+          )
+    			sendTransaction(
+    				stop_trans(
+    					'B', take_profit_pricet,
+    					math.abs(limits) / lot_size,
+    					stop_limit_price + 10 * price_step,
+    					stop_limit_price,
+    					asset, price_step
+    				)
+    			)
+        elseif value['bal'] > 0 then
+          stop_limit_price = (
+            value['position_price'] - dist_to_stop[key] * value['position_price']
+          )
+    			stop_limit_price = (
+            stop_limit_price - math.fmod(stop_limit_price, value['price_step'])
+          )
+    			take_profit_price = (
+            value['position_price'] + dist_to_stop[key] * value['position_price']
+          )
+    			take_profit_price = (
+            take_profit_price - math.fmod(take_profit_price, value['price_step'])
+          )
+          sendTransaction(
+    				stop_trans(
+    					'S', take_profit_price,
+    					math.abs(limits) / lot_size,
+    					stop_limit_price - 10 * price_step,
+    					stop_limit_price,
+    					asset, price_step
+    				)
+    			)
+        end
+
       end
     end
 		sleep(10000)
